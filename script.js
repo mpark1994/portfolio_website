52dd8701cd0b770b7c0f7cb7620109aa50449423
mpark1994 <mpark1994@outlook.com>
2023-01-25 07:09:05 -0500

    updated subscription logic

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ const emailSubmit = document.querySelector('#email-submit')
 
 myForm.addEventListener('submit', onSubmit)
 
+const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+  
+// This is a placeholder to emulate a subscription - if needed, make a functioning newsletter subscription.
 function onSubmit(e) {
     e.preventDefault()
     
-    if(emailInput.value != '') {
+    if (validateEmail(emailInput.value)) {
         emailInput.value = ''
         emailInput.placeholder = 'Thanks for Subscribing!'
         emailInput.setAttribute('disabled', '')
         emailSubmit.value = 'Subscribed!'
         emailSubmit.setAttribute('disabled', '')
     }
-
 }
